a515f48f1b590a88ea6abd00f17fd3ef393c2e74
NineWP <87454823+NineWP@users.noreply.github.com>
2022-02-09 13:32:08 +0700

    OOP java 2022 lab5

--- Lab5_64010761/Pro2_64010761.java
+++ Lab5_64010761/Pro2_64010761.java
@@ -1,15 +1,15 @@
 public class Pro2_64010761 {
     public static void main(String[] args) {
         RegularPolygon polygon1 = new RegularPolygon();
-        System.out.println("Perimeter : " + polygon1.getPerimeter());
+        System.out.println("Perimeter of polygon 1: " + polygon1.getPerimeter());
         System.out.printf("Area : %.2f\n", polygon1.getArea());
 
         RegularPolygon polygon2 = new RegularPolygon(6, 4);
-        System.out.println("Perimeter : " + polygon2.getPerimeter());
+        System.out.println("Perimeter of polygon 2: " + polygon2.getPerimeter());
         System.out.printf("Area : %.2f\n", polygon2.getArea());
 
         RegularPolygon polygon3 = new RegularPolygon(10, 4, 5.6, 7.8);
-        System.out.println("Perimeter : " + polygon3.getPerimeter());
+        System.out.println("Perimeter of polygon 3: " + polygon3.getPerimeter());
         System.out.printf("Area : %.2f\n", polygon3.getArea());
     }
 }
